2abddd484d03c83980fa2dd274a2c0a73c3c912a
medlaq777 <m.laqsoumi@gmail.com>
2025-12-08 22:24:02 +0100

    LOG-17 Create middleware Role

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -17,6 +17,24 @@ class AuthMiddleware {
     }
   }
 
-  
+  static authorizeRole(role) {
+    return (req, res, next) => {
+      try {
+        if (!req.user) {
+          const err = new Error("Unauthorized");
+          err.status = 401;
+          throw err;
+        }
+        if (req.user.role !== role) {
+          const err = new Error("Forbidden");
+          err.status = 403;
+          throw err;
+        }
+        next();
+      } catch (err) {
+        next(err);
+      }
+    };
+  }
 }
 export default AuthMiddleware;
