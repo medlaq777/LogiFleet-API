8f1944e58340e2edd7123de0944f3b15131dfa21
medlaq777 <m.laqsoumi@gmail.com>
2025-12-09 15:19:35 +0100

    Create truck service with all functions necessary

--- src/services/truck.service.js
+++ src/services/truck.service.js
@@ -47,18 +47,19 @@ class TruckService {
     return this.truckRepository.update(id, data);
   }
 
-  async deleteTruck(id){
+  async deleteTruck(id) {
     const truck = await this.truckRepository.findById(id);
     if (!truck) {
       const err = new Error("Truk Not Found");
       err.status = 404;
       throw err;
     }
-    if (data.status === "En service" && truck.driver === null) {
-      const err = new Error("Cannot update status without driver assigned");
+    if (data.status === "En service") {
+      const err = new Error("Cannot delete truck in service");
       err.status = 400;
       throw err;
     }
+    return this.truckRepository.delete(id);
   }
 }
 export default new TruckService(TruckRepository);
