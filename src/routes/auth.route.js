ed833aa886c4d60d463054d2a46405868cb7fbc6
medlaq777 <m.laqsoumi@gmail.com>
2025-12-09 10:07:11 +0100

    Update The AuthRoute with adding endpoint for get profile also add protect middleware for protect routes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,17 @@
 import express from "express";
 import AuthController from "../controllers/auth.controller.js";
+import AuthMiddleware from "../middlewares/auth.middleware.js";
 
 class AuthRoute {
   static build() {
     const router = express.Router();
     router.post("/register", AuthController.register.bind(AuthController));
     router.post("/login", AuthController.login.bind(AuthController));
-
+    router.get(
+      "/profile",
+      AuthMiddleware.protect,
+      AuthController.profile.bind(AuthController)
+    );
     return router;
   }
 }
