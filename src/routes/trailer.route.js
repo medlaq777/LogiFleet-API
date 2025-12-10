d102e32f71166136f5c57d3bc46fa96d7a8b0088
medlaq777 <m.laqsoumi@gmail.com>
2025-12-10 10:00:48 +0100

    LOG-47 Protect the Trailer routes with Auth and authorized role Admin and create all crud trailer routes

---
+++ src/routes/trailer.route.js
@@ -0,0 +1,30 @@
+import express from "express";
+import AuthMiddleware from "../middlewares/auth.middleware.js";
+import TrailerController from "../controllers/trailer.controller.js";
+
+class TrailerRoute {
+  static build() {
+    const router = express.Router();
+    router.use(AuthMiddleware.protect);
+    router.use(AuthMiddleware.authorizeRole("Admin"));
+    router.get(
+      "/trailers",
+      TrailerController.getAllTrailers.bind(TrailerController)
+    );
+    router.post(
+      "/trailers",
+      TrailerController.createTrailer.bind(TrailerController)
+    );
+    router.put(
+      "/trailers/:id",
+      TrailerController.updateTrailer.bind(TrailerController)
+    );
+    router.delete(
+      "/trucks/:id",
+      TrailerController.deleteTrailer.bind(TrailerController)
+    );
+    return router;
+  }
+}
+
+export default TrailerRoute;
