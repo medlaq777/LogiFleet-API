0d264be1fa58f02110189268b8ccb0f5d50cb8d4
medlaq777 <m.laqsoumi@gmail.com>
2025-12-10 15:15:27 +0100

    Create the Trip repository inherite from vehicle repository

---
+++ src/repositories/trip.repository.js
@@ -0,0 +1,24 @@
+import Trip from "../models/trip.model.js";
+import VehicleRepository from "./vehicle.repository.js";
+
+class TripRepository extends VehicleRepository {
+  constructor() {
+    super(Trip);
+  }
+  async findByDriverId(driverId) {
+    return this.model
+      .find({ driverId })
+      .populate("truckId", "licensePlate make model")
+      .populate("trailerId", "licensePlate")
+      .sort({ startDate: -1 });
+  }
+  async findByIdPopulated(id) {
+    return this.model
+      .findById(id)
+      .populate("driverId", "firstName lastName")
+      .populate("truckId", "licensePlate make model")
+      .populate("trailerId", "licensePlate");
+  }
+}
+
+export default new TripRepository();
